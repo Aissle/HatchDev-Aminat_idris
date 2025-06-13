678c93e337cb1fcea44a24a152841c6d2a9e0b22
Idris Aminat <idris.oaminat@gmail.com>
2025-06-13 16:37:04 +0100

    update assignment 8

--- 8_musicPlayer/music.ts
+++ 8_musicPlayer/music.ts
@@ -4,8 +4,8 @@
 // node
 class SongNode {
   song: string;
-  next: SongNode | null = null;
   prev: SongNode | null = null;
+  next: SongNode | null = null;
 
   constructor(Ivalue: string) {
     this.song = Ivalue;
@@ -19,12 +19,16 @@ class MusicPlayer {
 
   constructor(title: string, Ivalue: string) {
     this.title = title;
-    if (Ivalue == undefined) this.firstSong = null;
-    else {
+    if (Ivalue == undefined) {
+      this.firstSong = null;
+      console.log(`${this.title} music player created`);
+    } else {
       const head = new SongNode(Ivalue);
       this.firstSong = head;
+      console.log(
+        `${this.title} music player created, ${head} added to playlist`
+      );
     }
-    console.log(`${this.title} music player created`);
   }
 
   public addSong(song: string) {
@@ -49,8 +53,31 @@ class MusicPlayer {
         if (currentNode.song === song) break;
         currentNode = currentNode.next;
       }
-      currentNode!.next!.prev = currentNode!.prev;
-      currentNode!.prev!.next = currentNode!.next;
+      //if first song, but not only song
+      if (currentNode!.prev === null && currentNode!.next !== null) {
+        //console.log(currentNode);
+        this.firstSong = currentNode!.next;
+        //console.log(this.firstSong);
+        this.firstSong!.prev = null;
+        //console.log(this.firstSong);
+      }
+      // if last song but not only song
+      else if (currentNode!.next === null && currentNode!.prev !== null) {
+        //console.log(currentNode);
+        currentNode!.prev.next = null;
+        //console.log(currentNode);
+        currentNode!.prev = null;
+        //console.log(currentNode);
+      }
+      //if only song
+      else if (currentNode!.next === null && currentNode!.prev === null) {
+        this.firstSong = null;
+      }
+      //if neither first, nor last, nor only song - just a middle song
+      else {
+        currentNode!.next!.prev = currentNode!.prev;
+        currentNode!.prev!.next = currentNode!.next;
+      }
     }
     console.log(`song ${song} deleted from ${this.title} music player`);
   }
@@ -99,6 +126,38 @@ class MusicPlayer {
 
   public shuffle() {
     //let i = Math.random;
+    if (!this.firstSong) {
+      console.log("No songs to shuffle.");
+      return;
+    }
+
+    // Collect songs into an array
+    const nodes: SongNode[] = [];
+    let currentNode: SongNode | null = this.firstSong;
+    while (currentNode !== null) {
+      nodes.push(currentNode);
+      currentNode = currentNode.next;
+    }
+
+    //Shuffle the array using Math.random()
+    for (let i = nodes.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [nodes[i], nodes[j]] = [nodes[j], nodes[i]];
+    }
+
+    //Reconstruct linked list from shuffled nodes
+    this.firstSong = nodes[0];
+    this.firstSong.prev = null;
+
+    for (let i = 0; i < nodes.length - 1; i++) {
+      nodes[i].next = nodes[i + 1];
+      nodes[i + 1].prev = nodes[i];
+    }
+
+    nodes[nodes.length - 1].next = null;
+
+    console.log("Playlist shuffled:");
+    this.DisplayPlaylist();
   }
 
   public repeat(song: string) {
@@ -110,18 +169,66 @@ class MusicPlayer {
         if (currentNode.song === song) break;
         currentNode = currentNode.next;
       }
-      for (let i = 1; i <= 6; i++) {
-        console.log(`${currentNode!.song} is playing`);
+      if (currentNode === null)
+        console.log(`song ${song} not in ${this.title} music player`);
+      else {
+        for (let i = 1; i <= 6; i++) {
+          console.log(`${currentNode!.song} is playing`);
+        }
+        console.log("Done");
       }
-      console.log("Done");
     }
   }
 }
 
-//class usuing list
-// class MusicPlayer {
-//   playList: PlayList | null;
-//   constructor(playList: PlayList) {
-//     this.playList = playList;
-//   }
-// }
+function runTests() {
+  const player = new MusicPlayer("MyPlaylist", "Song1");
+
+  console.log("\n--- Add Songs Test ---");
+  player.addSong("Song2");
+  player.addSong("Song3");
+  player.addSong("Song4");
+  player.DisplayPlaylist();
+
+  console.log("\n--- Delete Song Test (middle song) ---");
+  player.deleteSong("Song2");
+  player.DisplayPlaylist();
+
+  console.log("\n--- Delete Song Test (first song) ---");
+  player.deleteSong("Song1");
+  player.DisplayPlaylist();
+
+  console.log("\n--- Delete Song Test (last song) ---");
+  player.deleteSong("Song4");
+  player.DisplayPlaylist();
+
+  console.log("\n--- Delete Song Test (last and only song) ---");
+  player.deleteSong("Song3");
+  player.DisplayPlaylist();
+
+  console.log("\n--- Add Songs Again ---");
+  player.addSong("Alpha");
+  player.addSong("Bravo");
+  player.addSong("Charlie");
+  player.DisplayPlaylist();
+
+  console.log("\n--- Play Songs (Forward) ---");
+  player.play();
+
+  console.log("\n--- Play Songs (Reverse) ---");
+  player.playReverse();
+
+  console.log("\n--- Repeat Test ---");
+  player.repeat("Bravo");
+
+  console.log("\n--- Repeat Non-existent Song ---");
+  player.repeat("Delta");
+
+  console.log("\n--- Shuffle (structure only, not implemented) ---");
+  player.shuffle();
+
+  console.log("\n--- Final Playlist ---");
+  player.DisplayPlaylist();
+}
+
+runTests();
