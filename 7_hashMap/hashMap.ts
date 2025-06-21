d296b6a1ed1900e4dba02bb60c030dfaffc5ae75
Idris Aminat <idris.oaminat@gmail.com>
2025-06-21 15:02:37 +0100

    Update hashMap.ts

--- 7_hashMap/hashMap.ts
+++ 7_hashMap/hashMap.ts
@@ -1,1 +1,14 @@
-//  Assignmet 8. Build your hashmap, handle your own collision and create your own hash function
+//  Assignmet 7. Build your hashmap, handle your own collision and create your own hash function
+
+
+function hashCode(str: string): number {
+  let hash = 0;
+  if (str.length === 0) return hash;
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash |= 0;
+  }
+  return hash;
+}
+  
